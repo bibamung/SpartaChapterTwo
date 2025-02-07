8c71318816303705b6a27bf138769b9fad183788
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-02-07 15:30:26 +0900

    [ Refactor ] 몬스터 파트와 머지 후 연결 작업중

--- Sylphyr/Sylphyr/GameManger.cs
+++ Sylphyr/Sylphyr/GameManger.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sylphyr.KJE;
 
 namespace Sylphyr
 {
     public class GameManger : SingleTon<GameManger>
     {
-        public Player player { get; } = new Player();
+        // public Player player { get; } = new Player();
     }
 }

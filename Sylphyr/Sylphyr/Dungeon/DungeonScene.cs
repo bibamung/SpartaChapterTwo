b22d8dfb94115ffdc1fbede87a765ebf946b988f
Kim Seong Won <worni5015@gmail.com>
2025-02-07 20:26:26 +0900

    [ Feat ] Monsters by Stage Appearance
    스테이지 별 몬스터 등장하도록 만듦

--- Sylphyr/Sylphyr/Dungeon/DungeonScene.cs
+++ Sylphyr/Sylphyr/Dungeon/DungeonScene.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Sylphyr.KJE;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -16,50 +19,40 @@ namespace Sylphyr.Dungeon
             bool isValidNum = int.TryParse(Console.ReadLine(), out stage);
             if (isValidNum)
             {
-                switch (stage)
+                if (stage > 0 && stage < 50)
                 {
-                    case 1:
-                        dungeonManager.DungeonStart(stage);
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        break;
-                    case 7:
-                        break;
-                    case 8:
-                        break;
-                    case 9:
-                        break;
-                    case 10:
-                        break;
-                    case 11:
-                        break;
-                    case 12:
-                        break;
-                    case 13:
-                        break;
-                    case 14:
-                        break;
-                    case 15:
-                        break;
-                    default:
-                        break;
+                    dungeonManager.DungeonStart(stage);
                 }
             }
             else
             {
                 Console.WriteLine("잘못 입력하셨습니다.");
             }
-
-
             return stage;
+        }
+
+        public void DisplayPlayerHpBar(Player player)
+        {
+
+            int count = 1;
+            int barSize = 20; // 체력바 길이 (20칸)
+            float healthPercentage = player.CurrentHp / player.TotalStat.MaxHp;
+            int filledBars = (int)(barSize * healthPercentage);
+            int emptyBars = barSize - filledBars;
+
+            // 색상 적용 (콘솔 전용)
+            Console.ForegroundColor = GetHealthColor(healthPercentage);
+
+            // 체력바 출력
+            Console.Write($"{count}. {player.Name} [");
+            Console.Write(new string('■', filledBars)); // 채워진 부분
+            Console.Write(new string('□', emptyBars));  // 빈 부분
+            Console.Write($"] {player.CurrentHp}/{player.TotalStat.MaxHp}");
+
+            // 색상 초기화
+            Console.ResetColor();
+            Console.WriteLine();
+
         }
 
         public void DisplayHealthBar(List<Monster> stageMonsters)
@@ -78,14 +71,115 @@ namespace Sylphyr.Dungeon
                 // 체력바 출력
                 Console.Write($"{count}. {monster.MonsterName} [");
                 Console.Write(new string('■', filledBars)); // 채워진 부분
-                Console.Write(new string('-', emptyBars));  // 빈 부분
+                Console.Write(new string('□', emptyBars));  // 빈 부분
                 Console.Write($"] {monster.Hp}/{monster.MaxHp}");
 
                 // 색상 초기화
                 Console.ResetColor();
                 Console.WriteLine();
             }
-            
+
+        }
+
+        public void DisplayHealthBar(Monster monster)
+        {
+
+            int barSize = 20; // 체력바 길이 (20칸)
+            float healthPercentage = monster.Hp / monster.MaxHp;
+            int filledBars = (int)(barSize * healthPercentage);
+            int emptyBars = barSize - filledBars;
+
+            // 색상 적용 (콘솔 전용)
+            Console.ForegroundColor = GetHealthColor(healthPercentage);
+
+            // 체력바 출력
+            Console.Write($"{monster.MonsterName} [");
+            Console.Write(new string('■', filledBars)); // 채워진 부분
+            Console.Write(new string('□', emptyBars));  // 빈 부분
+            Console.Write($"] {monster.Hp}/{monster.MaxHp}");
+
+            // 색상 초기화
+            Console.ResetColor();
+            Console.WriteLine();
+
+        }
+
+        //플레이어가 맞았을 때
+        public void DisplayHit(Monster monster, Player player, bool isCritical, float finalDamage)
+        {
+            if (isCritical) //크리티컬이 터졌습니다.
+            {
+                Console.WriteLine($"{monster.MonsterName}이/가 {player.Name}을 공격했다.");
+                Console.WriteLine($"효과는 굉장했다.");
+                Console.WriteLine($"{player.Name}에게 {finalDamage}만큼 피해를 입혔다.");
+                DisplayPlayerHpBar(player);
+
+                Console.WriteLine("계속 진행하시려면 Enter키를 눌러주세요...");
+                Console.ReadLine();
+            }
+            else            //크리티컬이 안 터졌습니다.
+            {
+                Console.WriteLine($"{monster.MonsterName}이/가 {player.Name}을 공격했다.");
+                Console.WriteLine($"{player.Name}에게 {finalDamage}만큼 피해를 입혔다.");
+                DisplayPlayerHpBar(player);
+
+                Console.WriteLine("계속 진행하시려면 Enter키를 눌러주세요...");
+                Console.ReadLine();
+            }
+        }
+
+        //플레이어가 때렸을 때
+        public void DisplayHit(Player player, Monster monster, bool isCritical, float finalDamage)
+        {
+            if (isCritical) //크리티컬이 터졌습니다.
+            {
+                Console.WriteLine($"{monster.MonsterName}를 공격했다.");
+                Console.WriteLine($"효과는 굉장했다.");
+                Console.WriteLine($"{monster.MonsterName}에게 {finalDamage}만큼 피해를 입혔다.");
+                monster.Hp -= finalDamage;
+                DisplayHealthBar(monster);
+
+                Console.WriteLine("계속 진행하시려면 Enter키를 눌러주세요...");
+                Console.ReadLine();
+            }
+            else            //크리티컬이 안 터졌습니다.
+            {
+                Console.WriteLine($"{monster.MonsterName}를 공격했다.");
+                Console.WriteLine($"{monster.MonsterName}에게 {finalDamage}만큼 피해를 입혔다.");
+                monster.Hp -= finalDamage;
+                DisplayHealthBar(monster);
+
+                Console.WriteLine("계속 진행하시려면 Enter키를 눌러주세요...");
+                Console.ReadLine();
+            }
+        }
+
+
+        //플레이어가 때린걸 몬스터가 회피했을때
+        public void DisplayEvasion(Player player)
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+            int text = rand.Next(0, 4);
+            switch (text)
+            {
+                case 1:
+                    Console.WriteLine($"{player.Name}의 공격이 빗나갔다.");
+                    break;
+                case 2:
+                    Console.WriteLine($"{player.Name}가 멍 때리고 있어 공격을 하지 않았다.");
+                    break;
+                case 3:
+                    Console.WriteLine($"{player.Name}가 공격을 하다가 돌부리에 걸려 넘어져 공격에 실패했다.");
+                    break;
+            }
+
+            Console.WriteLine("계속 진행하시려면 Enter키를 눌러주세요...");
+            Console.ReadLine();
+        }
+
+        public void DisplayEvasion(Monster monster, Player player)
+        {
+
         }
 
         // 체력 상태에 따라 색상 변경 (콘솔용)
@@ -97,5 +191,5 @@ namespace Sylphyr.Dungeon
         }
 
     }
-    
+
 }
--- Sylphyr/Sylphyr/GameManger.cs
+++ Sylphyr/Sylphyr/GameManger.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sylphyr.KJE;
 
 namespace Sylphyr
 {
     public class GameManger : SingleTon<GameManger>
     {
-        public Player player { get; } = new Player();
+        public Player player { get; } = new Player("성원",CharacterClass.Thief);
     }
 }

f2833d2ae0baaf069594ab4fb73e0be6d19a2878
kgs1121 <kkggss1121@gmail.com>
2025-02-07 19:04:00 +0900

    [feat] 인벤토리, 상점 구매

---
+++ Sylphyr/Sylphyr/Inventory.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylphyr
+{
+    public class Inventory
+    {
+        public string statname;
+        
+
+        // 인벤토리에 담을 아이템 리스트
+        private List<Item> invenitems = new List<Item>();
+
+        // 인벤토리에서 아이템 추가
+        public void AddItem(Item item)
+        {
+            invenitems.Add(item);
+            Console.WriteLine($"{item.Name}이(가) 인벤토리에 추가되었습니다.");
+        }
+
+        // 인벤토리에서 아이템 제거
+        public void RemoveItem(Item item)
+        {
+            invenitems.Remove(item);
+            Console.WriteLine($"{item.Name}이(가) 인벤토리에서 제거되었습니다.");
+        }
+
+
+
+        // 인벤토리 메뉴
+        public void invenDisplay(Player player, bool isfail)  
+        {
+            Console.Clear();
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유한 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine();
+            if (invenitems.Count == 0)
+            {
+                Console.WriteLine("인벤토리가 비어 있습니다.");
+            }
+            
+            foreach (var item in invenitems)
+            {
+                string slotname = "";
+                string statname = "";
+
+                // 장비 부위 구분
+                if (item.Slot == "0")
+                    slotname = "상의";
+                else if (item.Slot == "1")
+                    slotname = "하의";
+                else if (item.Slot == "2")
+                    slotname = "신발";
+                else
+                    slotname = "장신구";
+
+                // 스텟 구분
+                if (item.Stat == 0) statname = "크리데미지";
+                else if (item.Stat == 1) statname = "민첩";
+                else if (item.Stat == 2) statname = "행운";
+                else if (item.Stat == 3) statname = "방어력";
+                string equippedText = item.isEquip ? "[E]" : "";
+                Console.WriteLine($"- {item.Name} | {statname} +{item.Stat2} | {slotname}  | 가격: {item.Price}G | 설명: {item.Desc}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("1. 장착 관리");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+
+            if (isfail)
+            {
+                Console.WriteLine("다시 입력해주세요.");
+                isfail = false;
+            }
+
+            int input = GetInput(0, 1);
+
+            switch (input)
+            {
+                case -1:
+                    isfail = true;
+                    break;
+                case 0:
+                    return;
+                case 1:
+                    EquipDisplay(player);
+                    break;
+            }
+            
+        }
+
+
+
+        // 장비 착용 해제 메뉴
+        public void EquipDisplay(Player player, bool isfail = false)   
+        {
+            Console.Clear();
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+
+            int i = 1;  // 아이템 장착 해제 번호
+
+            foreach (var item in invenitems)
+            {
+                string slotname = "";
+                string statname = "";
+
+                // 장비 부위 구분
+                if (item.Slot == "0") slotname = "상의";
+                else if (item.Slot == "1") slotname = "하의";
+                else if (item.Slot == "2") slotname = "신발";
+                else slotname = "장신구";
+
+                // 스텟 구분
+                if (item.Stat == 0) statname = "크리데미지";
+                else if (item.Stat == 1) statname = "민첩";
+                else if (item.Stat == 2) statname = "행운";
+                else statname = "방어력";
+
+                Console.WriteLine($"- {i}. {item.Name} | {statname} +{item.Stat2} | {slotname} | 가격: {item.Price}G | 설명: {item.Desc}");
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            if (isfail)
+            {
+                Console.WriteLine("다시 입력해주세요.");
+                isfail = false;
+            }
+
+            int input = GetInput(0, invenitems.Count);
+            var selectedItem = invenitems[input - 1];  // 번호에 맞는 아이템을 선택
+
+            switch (input)
+            {
+                case -1:
+                    isfail = true;
+                    break;
+                case 0:
+                    return;
+                default:
+                    if (!selectedItem.isEquip)
+                    {
+                        selectedItem.isEquip = true;  // 아이템 장착
+                        switch (selectedItem.Stat)
+                        {
+                            case 0:
+                                player.criticalDamage += selectedItem.Stat2;
+                                break;
+                            case 1:
+                                player.dex += selectedItem.Stat2;
+                                break;
+                            case 2:
+                                player.luk += selectedItem.Stat2;
+                                break;
+                            case 3:
+                                player.def += selectedItem.Stat2;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        selectedItem.isEquip = false;  // 아이템 해제
+                        switch (selectedItem.Stat)
+                        {
+                            case 0:
+                                player.criticalDamage -= selectedItem.Stat2;
+                                break;
+                            case 1:
+                                player.dex -= selectedItem.Stat2;
+                                break;
+                            case 2:
+                                player.luk -= selectedItem.Stat2;
+                                break;
+                            case 3:
+                                player.def -= selectedItem.Stat2;
+                                break;
+                        }
+                    }
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static int GetInput(int min, int max)
+        {
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.WriteLine(">> ");
+            if(int.TryParse(Console.ReadLine(), out int input) && input >= min && input <= max) return input;
+            else return -1;
+        }
+    }
+}
---
+++ Sylphyr/Sylphyr/Item.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylphyr
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Stat { get; set; }
+        public int Stat2 { get; set; }
+        public int Price { get; set; }
+        public string Slot { get; set; }
+        public string Desc { get; set; }
+        public bool isEquip { get; set; }  // 장착 여부
+
+        private static Dictionary<int, Item> ItemsDictionary = new Dictionary<int, Item>();
+
+
+        public Item(int id, string name, int stat, int stat2, string slot, int price, string desc, bool isequip)
+        {
+            ID = id;
+            Name = name;
+            Stat = stat;
+            Stat2 = stat2;
+            Slot = slot;
+            Price = price;
+            Desc = desc;
+            isEquip = isequip;
+        }
+
+
+        // 테스트용 아이템 추가
+        public void addTestItems()
+        {
+            ItemsDictionary = new Dictionary<int, Item>
+            {
+                { 1000, new Item(1000, "낡은 로브", 2, 10, "상의", 500, "줘도 안 입을 로브, 냄새도 나는 것 같다.", false) },
+                { 1001, new Item(1001, "강철 검", 10, 5, "무기", 1500, "강한 검이다.", false) },
+                { 1002, new Item(1002, "마법 지팡이", 8, 12, "하의", 2000, "마법을 증폭시키는 지팡이.", false) },
+                { 1003, new Item(1003, "가죽 방어구", 5, 10, "장신구", 800, "가죽으로 만든 방어구.", false) }
+            };
+        }
+
+        // 상점에서 아이템 리스트 출력
+        public void shopScene(Player player, Inventory inventory, bool isfail = false)
+        {
+            Console.Clear();
+            Console.WriteLine("상점\n");
+            Console.WriteLine("[아이템 목록]\n");
+
+            // 아이템 목록을 번호를 붙여서 출력
+            foreach (var item in ItemsDictionary.Values)
+            {
+                string slotname = "";
+                string statname = "";
+                // 장비 부위 구분
+                if (item.Slot == "0") slotname = "상의";
+                else if (item.Slot == "1") slotname = "하의";
+                else if (item.Slot == "2") slotname = "신발";
+                else slotname = "장신구";
+
+                // 스텟 구분
+                if (item.Stat == 0) statname = "크리데미지";
+                else if (item.Stat == 1) statname = "민첩";
+                else if (item.Stat == 2) statname = "행운";
+                else if (item.Stat == 3) statname = "방어력";
+
+
+                Console.WriteLine($"- {item.Name} | {statname} +{item.Stat2} | 슬롯: {slotname} | 가격: {item.Price}G | 설명: {item.Desc}");
+            }
+
+            // 메뉴 선택
+            Console.WriteLine("\n1. 아이템 구매");
+            Console.WriteLine("2. 아이템 판매");
+            Console.WriteLine("0. 나가기");
+
+
+            int input = GetInput(0, 2);   // 행동값
+
+            if (isfail) Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+
+            switch (input)
+            {
+                case 1:
+                    BuyItem(player, inventory);
+                    break;
+                case 2:
+
+                    break;
+                case 0:
+                    Console.WriteLine("상점을 나갑니다.");
+                    return;
+                default:
+                    isfail = true;
+                    break;
+            }
+        }
+
+        // 아이템 구매 로직
+        public void BuyItem(Player player, Inventory inventory, bool isfail = false)
+        {
+           
+            Console.Clear();
+            Console.WriteLine("상점\n");
+            Console.WriteLine("[아이템 목록]\n");
+            Console.WriteLine();
+            
+            int index = 1;  // 아이템 구매 번호
+
+            foreach (var item in ItemsDictionary.Values)
+            {
+                string slotname = "";
+                string statname = "";
+
+                // 장비 부위 구분
+                if (item.Slot == "0") slotname = "상의";
+                else if (item.Slot == "1") slotname = "하의";
+                else if (item.Slot == "2") slotname = "신발";
+                else slotname = "장신구";
+
+                // 스텟 구분
+                if (item.Stat == 0) statname = "크리데미지";
+                else if (item.Stat == 1) statname = "민첩";
+                else if (item.Stat == 2) statname = "행운";
+                else if (item.Stat == 3) statname = "방어력";
+
+                Console.WriteLine($"- {index}. {item.Name} | {statname} +{item.Stat2} | 슬롯: {slotname} | 가격: {item.Price}G | 설명: {item.Desc}");
+                index++;
+            }
+            Console.WriteLine("0. 나가기");
+
+            if (isfail)
+            {
+                Console.WriteLine("다시 입력해주세요.");
+                isfail = false;
+            }
+
+            int input = GetInput(0, ItemsDictionary.Count); // 번호 선택
+            var selectedItem = ItemsDictionary.Values.ToList()[input - 1]; // 번호는 1부터 시작하므로 -1
+
+            
+            switch (input)
+            {
+                case -1:
+                    isfail = true;
+                    break;
+                case 0:
+                    return;
+                default:
+                    if (player.Gold >= selectedItem.Price)
+                    {
+                        player.Gold -= selectedItem.Price;
+                        inventory.AddItem(selectedItem);
+                        
+                    }
+                    else isfail = true;
+                    break;
+            }
+        }
+    
+
+
+
+
+
+
+
+        // 사용자 입력 받기
+        private static int GetInput(int min, int max)
+        {
+            int input;
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.WriteLine(">> ");
+                if (int.TryParse(Console.ReadLine(), out input) && (input < min) && (input > max)) return input;
+
+            }
+        }
+    }
+}

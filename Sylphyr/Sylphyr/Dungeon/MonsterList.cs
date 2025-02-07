f3730e5d5569315062ab46c44003837c5b01f418
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-02-07 15:40:54 +0900

    Merge branch 'dev' into feature/kje/Player

---
+++ Sylphyr/Sylphyr/Dungeon/MonsterList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sylphyr.Dungeon
+{
+    public class MonsterList
+    {
+        Random random = new Random(DateTime.Now.Millisecond); //DateTime.Now.Millisecond
+        public List<Monster> MonsterLists { get; set; }
+
+        public MonsterList()
+        {
+            //Monster(int monsterId, string monsterName, float MaxHp, float hp, float atk, float def, float luk, float critical, float dex, float criticalChance, float critcalDamage, int speed, int dropGold, float dropExp)
+            //슬라임, 고블린, 홉고블린 오우거 오크 리자드 드래곤 스켈레톤 좀비
+            MonsterLists = new List<Monster>()
+            {
+                new Monster(1000,"슬라임",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1001,"고블린",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1002,"홉고블린",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1003,"오우거",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1004,"오크",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1005,"리자드",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1006,"스켈레톤",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1007,"좀비",100,100,5,5,5,0.1f,5,5,5,5,5,5),
+                new Monster(1008,"드래곤",100,100,5,5,5,0.1f,5,5,5,5,5,5)
+            };
+
+        }
+        public List<Monster> GetRandomMonsters(int stage)
+        {
+            int monsterCount = random.Next(1, 5); // 1~4개 등장
+            List<Monster> selectedMonsters = new List<Monster>();
+
+            // 몬스터를 랜덤하게 선택하면서 중복을 최대 2번까지만 허용
+            while (selectedMonsters.Count < monsterCount)
+            {
+                Monster randomMonster = MonsterLists[random.Next(MonsterLists.Count)];
+                int existingCount = selectedMonsters.Count(m => m.MonsterId == randomMonster.MonsterId);
+
+                if (existingCount < 2) // 같은 몬스터가 2번 미만일 때 추가
+                {
+                    selectedMonsters.Add(randomMonster);
+                }
+            }
+
+            return selectedMonsters;
+        }
+    }
+}
---
+++ Sylphyr/Sylphyr/Monster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylphyr
+{
+    public class Monster
+    {
+        public int MonsterId { get; set; }
+        public string MonsterName { get; set; }
+        public float Hp {  get; set; }
+        public float MaxHp { get; set; }
+        public float Atk { get; set; }
+        public float Def { get; set; }
+        public float Luk { get; set; }
+        public float Dex { get; set; }
+        
+        public float CriticalChance { get; set; }
+        public float CritcalDamage { get; set; }
+        public int Speed { get; set; }
+
+        int DropGold { get; set; }
+        float DropExp { get; set; }
+        
+        public Monster(int monsterId, string monsterName, float MaxHp, float hp, 
+            float atk, float def, float luk, float critical, float dex, float criticalChance,
+            float critcalDamage, int speed, int dropGold, float dropExp)
+        {
+            MonsterId = monsterId;
+            MonsterName = monsterName;
+            Hp = hp;
+            Atk = atk;
+            Def = def;
+            Luk = luk;
+            CriticalChance = criticalChance;
+            CritcalDamage = critcalDamage;
+            Dex = dex;
+            Speed = speed;
+            DropGold = dropGold;
+            DropExp = dropExp;
+        }
+    }
+}

95ac668f612f45a9f70289056d8060a85a6cdddf
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-02-13 10:33:38 +0900

    Update MainScene.cs

--- Sylphyr/Sylphyr/Scene/MainScene.cs
+++ Sylphyr/Sylphyr/Scene/MainScene.cs
@@ -16,13 +16,13 @@ public class MainScene
     {
         sb.AppendLine(" 빛의 마을 루미에라에 오신 것을 환영합니다!");
         sb.AppendLine();
-        sb.AppendLine("1. 상태 보기");
-        sb.AppendLine("2. 인벤토리");
-        sb.AppendLine("3. 상점");
-        sb.AppendLine("4. 던전 입장");
-        sb.AppendLine("5. 길드 입장");
-        sb.AppendLine("6. 저장하기");
-        sb.AppendLine("0. 게임 종료");
+        sb.AppendLine(" 1. 상태 보기");
+        sb.AppendLine(" 2. 인벤토리");
+        sb.AppendLine(" 3. 상점");
+        sb.AppendLine(" 4. 던전 입장");
+        sb.AppendLine(" 5. 길드 입장");
+        sb.AppendLine(" 6. 저장하기");
+        sb.AppendLine(" 0. 게임 종료");
         sb.AppendLine();
     }
 
